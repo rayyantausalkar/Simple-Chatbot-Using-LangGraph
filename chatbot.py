3179906f6f5db02bc4f03790acc48e23ce5adcb5
Rayyan Tausalkar <rtausalkar@gmail.com>
2026-06-24 18:47:55 +0530

    Implement sqlitesaver checkpointer replacing inmemorysaver to store the chats

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,22 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import BaseMessage, add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+
 from typing import TypedDict, Annotated
+import sqlite3
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
 # model
-model = ChatGoogleGenerativeAI(
-    model='gemini-2.5-flash',
-    temperature='0.5',
+model = ChatOpenAI(
+    base_url="http://localhost:1234/v1",
+    api_key="lm-studio",
+    model="google/gemma-4-e4b",
+    temperature=0.5
 )
 
 # state
@@ -20,7 +25,9 @@ class ChatbotState(TypedDict):
 
 # graph
 graph = StateGraph(ChatbotState)
-checkpointer = InMemorySaver()
+
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 
 # functions
 def chat_feature(state: ChatbotState) -> ChatbotState:
@@ -37,3 +44,9 @@ graph.add_edge('chat_feature', END)
 
 # compile the graph
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    return list(all_threads)
